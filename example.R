0967168f9c6aa6de07e9c21410fd249132ee835f
Paul Eves <soniccorpse@gmail.com>
2021-01-19 09:45:46 +0000

    Tweak example file

--- example.R
+++ example.R
@@ -1,6 +1,6 @@
 # Demo usage file
 
-# source The qurey function file
+# source The query function file
 source("./os_api_query.R")
 
 # import the secret API key
@@ -9,8 +9,8 @@ KEY <-readLines("./keyfile")
 # call get_address
 result <-get_address("133","SE1 8UG", KEY)
 result1 <-get_address("Wellington House","SE1 8UG", KEY)
-result2 <-get_address("15","SO51 0DP", KEY)
-result3 <-get_address("79","SO15 4JU", KEY)
+result2 <-get_address("10","SW1A 2AA", KEY)
+result3 <-get_address("61","NW9 5DF", KEY)
 result4 <-get_address("99","SO01 0ZZ", KEY)
 result5 <-get_address("99","SO01 0ZZ", KEY, "Nothing Found")
 result6 <-get_address("the meadows","SP5 1EZ", KEY, "Nothing Found")
