994101cf7df8f02edbadb9829ad52be3be7c4b93
Paul Eves <soniccorpse@gmail.com>
2021-01-19 13:36:30 +0000

    Whitespace handling (#4)
    * Modified to ignore whitespace in postcodes

--- example.R
+++ example.R
@@ -8,16 +8,15 @@ KEY <-readLines("./keyfile")
 
 # call get_address
 result <-get_address("133","SE1 8UG", KEY)
+resulta <-get_address("133","SE18UG", KEY)
 result1 <-get_address("Wellington House","SE1 8UG", KEY)
+result1a <-get_address("Wellington House","SE18UG", KEY)
 result2 <-get_address("10","SW1A 2AA", KEY)
 result3 <-get_address("61","NW9 5DF", KEY)
 result4 <-get_address("99","SO01 0ZZ", KEY)
 result5 <-get_address("99","SO01 0ZZ", KEY, "Nothing Found")
 result6 <-get_address("the meadows","SP5 1EZ", KEY, "Nothing Found")
 
-output <- c(result,result1,result2,result3,result4,result5,result6)
+output <- c(result,resulta,result1,result1a,
+            result2,result3,result4,result5,result6)
 paste(output)
-
-
-
-
