717ebea82a73cb8460e7191ada525b6bc55374b1
Paul Eves <soniccorpse@gmail.com>
2021-01-19 18:31:01 +0000

    Improve Matching and include sub building name Resolves #5. factored out addition of default missing columns into own function add_missing_columns. added SUB_BUILDING_NAME to the data frame and subsequent match logic. refactored building number search to account for first line of address or other junk included in the parameter to the function.

--- example.R
+++ example.R
@@ -20,3 +20,12 @@ result6 <-get_address("the meadows","SP5 1EZ", KEY, "Nothing Found")
 output <- c(result,resulta,result1,result1a,
             result2,result3,result4,result5,result6)
 paste(output)
+
+
+a = get_address("6","RG13FF",KEY) # solve by looking at SUB_BUILDING_NAME (it's a flat number)
+b = get_address("FORGE COTTAGE","CA110RD",KEY) # as previous
+c = get_address("375 BUSHBURY LANE","WV108JZ",KEY) # this one is a number and a road name. Would work if you looked for building number
+
+output2 <- c(a,b,c)
+paste(output)
+paste(output2)
